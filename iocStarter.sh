fb06384eaa4cf63dbd157b16e0b2340c361d72f0
Pete Jemian <prjemian@gmail.com>
2021-02-23 10:00:11 -0600

    MNT rename

---
+++ iocStarter.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# file: iocStarter.sh
+# purpose: start EPICS IOCs for 2021-03 training
+
+start_xxx.sh ioc
+start_adsim.sh ad
--- start_training_iocs.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-start_xxx.sh ioc
-start_adsim.sh ad
