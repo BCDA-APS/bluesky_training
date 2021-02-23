4fc2e12957408bd05f338c7b2cbb0b8e3bbe46a8
Pete Jemian <prjemian@gmail.com>
2021-02-23 15:30:27 -0600

    MNT starters

--- blueskyStarter.sh
+++ blueskyStarter.sh
@@ -9,13 +9,45 @@ export CONDA_ENVIRONMENT=bluesky_2021_1
 # bluesky
 
 export IPYTHON_PROFILE=bluesky
-export IPYTHONDIR=/home/prjemian/.ipython-bluesky
+export IPYTHONDIR="${HOME}/.ipython-bluesky"
 
-export OPTIONS=""
-export OPTIONS="${OPTIONS} --profile=${IPYTHON_PROFILE}"
-export OPTIONS="${OPTIONS} --ipython-dir=${IPYTHONDIR}"
-export OPTIONS="${OPTIONS} --IPCompleter.use_jedi=False"
-export OPTIONS="${OPTIONS} --InteractiveShellApp.hide_initial_ns=False"
+console_session () {
+    export OPTIONS=""
+    export OPTIONS="${OPTIONS} --profile=${IPYTHON_PROFILE}"
+    export OPTIONS="${OPTIONS} --ipython-dir=${IPYTHONDIR}"
+    export OPTIONS="${OPTIONS} --IPCompleter.use_jedi=False"
+    export OPTIONS="${OPTIONS} --InteractiveShellApp.hide_initial_ns=False"
 
-source ${CONDA_ACTIVATE} ${CONDA_ENVIRONMENT}
-ipython ${OPTIONS}
+    source ${CONDA_ACTIVATE} ${CONDA_ENVIRONMENT}
+    ipython ${OPTIONS}
+}
+
+lab_server () {
+    export OPTIONS=""
+    # export OPTIONS="${OPTIONS} --no-browser"
+    export OPTIONS="${OPTIONS} --ip=${HOST}"
+
+    source ${CONDA_ACTIVATE} ${CONDA_ENVIRONMENT}
+
+    python -m ipykernel install --user --name "${CONDA_ENVIRONMENT}"
+    jupyter-lab ${OPTIONS}
+}
+
+# start_iocs () {
+#     # FIXME: needs path/to/script (if not in ~/bin)
+#     iocStarter.sh
+# }
+
+usage () {
+    echo $"Usage: $0 [lab | help]"
+}
+
+case $(echo "${1}" | tr '[:upper:]' '[:lower:]') in
+  gui | jupyter | lab | notebook | server)  lab_server  ;;
+  "" | console | ipython)  console_session  ;;
+  # docker | ioc)  start_iocs  ;;
+  help | usage)  usage  ;;
+  *)
+    usage
+    exit 1
+esac
--- iocStarter.sh
+++ iocStarter.sh
@@ -3,5 +3,5 @@
 # file: iocStarter.sh
 # purpose: start EPICS IOCs for 2021-03 training
 
-start_xxx.sh ioc
+start_xxx.sh gp
 start_adsim.sh ad
