58fde80d5e8d011fce4a5fb7399ab056482a1cbf
Pete Jemian <prjemian@gmail.com>
2021-02-24 13:12:13 -0600

    MNT metadata

--- instrument/plans/peak_finder_example.py
+++ instrument/plans/peak_finder_example.py
@@ -25,7 +25,7 @@ import apstools.utils
 import pyRestTable
 
 
-def example1():
+def example1(md=None):
     """
     Find the peak of noisy v. m1 in the range of +/- 2.
 
@@ -48,18 +48,22 @@ def example1():
     4. add this plan's name to ``__all__`` list at top
     5. changed bp.scan to bp.rel_scan
     """
+    md = md or {}
+
     sig = 2
     m1.move(0)
-    yield from bp.rel_scan([noisy], m1, -sig, +sig, 23)
+    md["scan_sequence"] = 1
+    yield from bp.rel_scan([noisy], m1, -sig, +sig, 23, md=md)
     sig = peaks["fwhm"]["noisy"]
     m1.move(peaks["cen"]["noisy"])
 
-    yield from bp.rel_scan([noisy], m1, -sig, +sig, 23)
+    md["scan_sequence"] = 2
+    yield from bp.rel_scan([noisy], m1, -sig, +sig, 23, md=md)
     sig = peaks["fwhm"]["noisy"]
     m1.move(peaks["cen"]["noisy"])
 
 
-def example_findpeak(number_of_scans=4, number_of_points=23):
+def example_findpeak(number_of_scans=4, number_of_points=23, md=None):
     """
     find peak of noisy v. m1 by repeated scans with refinement
 
@@ -72,13 +76,16 @@ def example_findpeak(number_of_scans=4, number_of_points=23):
         m1.move(peaks["cen"]["noisy"])
         RE(bp.rel_scan([noisy], m1, -fwhm, fwhm, 23))
     """
+    md = md or {}
+    md["number_of_scans"] = number_of_scans
     k = 1.5  # range expansion factor
     fwhm = 2.1 / k
     cen = 0
     results = []
     for _again in range(number_of_scans):
+        md["scan_sequence_number"] = _again+1
         m1.move(cen)
-        yield from bp.rel_scan([noisy], m1, -k * fwhm, k * fwhm, number_of_points)
+        yield from bp.rel_scan([noisy], m1, -k * fwhm, k * fwhm, number_of_points, md=md)
         if "noisy" not in peaks["fwhm"]:
             logger.error("no data in `peaks`, end of these scans")
             break
@@ -93,13 +100,15 @@ def example_findpeak(number_of_scans=4, number_of_points=23):
     logger.info("iterative results:\n%s", str(tbl))
 
 
-def repeat_findpeak(iters=1):
+def repeat_findpeak(iters=1, md=None):
     # bec.disable_plots()
     # If plots are disabled, then the peak stats are not run
     # so peak finding fails.
+    md = md or {}
     for _i in range(iters):
+        md["iteration"] = _i+1
         apstools.utils.trim_plot_lines(bec, 4, m1, noisy)
         change_noisy_parameters()
-        yield from example_findpeak()
+        yield from example_findpeak(md=md)
         logger.info("Finished #%d of %d iterations", _i + 1, iters)
     # bec.enable_plots()
