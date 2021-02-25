febeefce0f446a0af5cd9ff6aeb827cb5166810a
Pete Jemian <prjemian@gmail.com>
2021-02-24 21:37:52 -0600

    MNT rename avoids import confusion

--- instrument/devices/__init__.py
+++ instrument/devices/__init__.py
@@ -8,7 +8,7 @@ from .area_detector import *
 from .calculation_records import *
 from .ioc_stats import *
 from .motors import *
-from .noisy import *
+from .noisy_detector import *
 from .scaler import *
-from .shutter import *
+from .shutter_simulator import *
 from .temperature_signal import *
