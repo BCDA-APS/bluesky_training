e267cf7837a279c0e01b938d58694b1db9516356
Pete Jemian <prjemian@gmail.com>
2021-02-24 22:50:06 -0600

    ENH change m1 SREV

--- instrument/devices/motors.py
+++ instrument/devices/motors.py
@@ -13,22 +13,27 @@ from ..session_logs import logger
 
 logger.info(__file__)
 
-from ophyd import EpicsMotor
+from ophyd import EpicsMotor, Component, EpicsSignal
 
+class MyEpicsMotor(EpicsMotor):
+    steps_per_revolution = Component(EpicsSignal, ".SREV", kind="omitted")
 
-m1 = EpicsMotor("gp:m1", name="m1", labels=("motor",))
-m2 = EpicsMotor("gp:m2", name="m2", labels=("motor",))
-m3 = EpicsMotor("gp:m3", name="m3", labels=("motor",))
-m4 = EpicsMotor("gp:m4", name="m4", labels=("motor",))
-m5 = EpicsMotor("gp:m5", name="m5", labels=("motor",))
-m6 = EpicsMotor("gp:m6", name="m6", labels=("motor",))
-m7 = EpicsMotor("gp:m7", name="m7", labels=("motor",))
-m8 = EpicsMotor("gp:m8", name="m8", labels=("motor",))
-m9 = EpicsMotor("gp:m9", name="m9", labels=("motor",))
-m10 = EpicsMotor("gp:m10", name="m10", labels=("motor",))
-m11 = EpicsMotor("gp:m11", name="m11", labels=("motor",))
-m12 = EpicsMotor("gp:m12", name="m12", labels=("motor",))
-m13 = EpicsMotor("gp:m13", name="m13", labels=("motor",))
-m14 = EpicsMotor("gp:m14", name="m14", labels=("motor",))
-m15 = EpicsMotor("gp:m15", name="m15", labels=("motor",))
-m16 = EpicsMotor("gp:m16", name="m16", labels=("motor",))
+m1 = MyEpicsMotor("gp:m1", name="m1", labels=("motor",))
+m2 = MyEpicsMotor("gp:m2", name="m2", labels=("motor",))
+m3 = MyEpicsMotor("gp:m3", name="m3", labels=("motor",))
+m4 = MyEpicsMotor("gp:m4", name="m4", labels=("motor",))
+m5 = MyEpicsMotor("gp:m5", name="m5", labels=("motor",))
+m6 = MyEpicsMotor("gp:m6", name="m6", labels=("motor",))
+m7 = MyEpicsMotor("gp:m7", name="m7", labels=("motor",))
+m8 = MyEpicsMotor("gp:m8", name="m8", labels=("motor",))
+m9 = MyEpicsMotor("gp:m9", name="m9", labels=("motor",))
+m10 = MyEpicsMotor("gp:m10", name="m10", labels=("motor",))
+m11 = MyEpicsMotor("gp:m11", name="m11", labels=("motor",))
+m12 = MyEpicsMotor("gp:m12", name="m12", labels=("motor",))
+m13 = MyEpicsMotor("gp:m13", name="m13", labels=("motor",))
+m14 = MyEpicsMotor("gp:m14", name="m14", labels=("motor",))
+m15 = MyEpicsMotor("gp:m15", name="m15", labels=("motor",))
+m16 = MyEpicsMotor("gp:m16", name="m16", labels=("motor",))
+
+m1.wait_for_connection()
+m1.steps_per_revolution.put(2000)
