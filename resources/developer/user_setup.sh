107410c8aeffa4349b70cb678b691f1ae260f6e0
Pete Jemian <prjemian@gmail.com>
2021-03-09 23:24:44 -0600

    ENH add test data

--- resources/developer/user_setup.sh
+++ resources/developer/user_setup.sh
@@ -5,22 +5,26 @@
 # run from root:  su -l -c /home/user_setup.sh - USERNAME
 
 #   su -l -s /bin/bash test3
-_developer_commands = """
 
-# must ALWAYS start this in the container
-service mongod start
-service mongod status
+# _developer_commands = """
 
-# "login" as user "test3"
-su -l -s /bin/bash test3
+# # must ALWAYS start this in the container
+# service mongod start
+# service mongod status
 
-# Are IOC prefixes defined?
-! env | grep -i ioc
+# # "login" as user "test3"
+# su -l -s /bin/bash test3
 
-import os
-os.environ.get("GP_IOC_PREFIX", "gp:")
+# # Are IOC prefixes defined?
+# ! env | grep -i ioc
 
-"""
+# import os
+# os.environ.get("GP_IOC_PREFIX", "gp:")
+
+# """
+
+############################################################
+# operating environment
 
 . /opt/miniconda3/bin/activate class_2021_03
 
@@ -33,6 +37,9 @@ cat >> ~/.bashrc << EOF
 export GP_IOC_PREFIX="gp${USER}:"
 export AD_IOC_PREFIX="ad${USER}:"
 EOF
+
+############################################################
+# IPython
 
 export IPYTHON_DIR=${HOME}/.ipython-bluesky
 
@@ -47,10 +54,16 @@ cat >> run_instrument.py << EOF
 from instrument.collection import *
 EOF
 
+############################################################
+# bash starter script
+
 cp /home/blueskyStarter.sh ./
 pushd ~/bin
 ln -s ${IPYTHON_DIR}/profile_bluesky/startup/blueskyStarter.sh ./
 popd
+
+############################################################
+# databroker configuration YAML file
 
 mkdir -p ~/.local/share/intake
 cat >> ~/.local/share/intake/databroker_mongodb.yml << EOF
@@ -61,3 +74,10 @@ sources:
       metadatastore_db: mongodb://localhost:27017/${USER}-bluesky
     driver: bluesky-mongo-normalized-catalog
 EOF
+
+############################################################
+# previously-collected data for analysis
+
+cd ~
+unzip /home/class_data_examples.zip
+databroker-unpack inplace "${HOME}/class_data_examples" class_data_examples
