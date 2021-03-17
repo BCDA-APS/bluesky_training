c14a3e4498cfa42b6293f2230d90b804ea2fd979
Pete R Jemian <prjemian@users.noreply.github.com>
2021-03-17 16:27:30 -0500

    MNT #4 naming of the export file

--- bluesky/export_data.sh
+++ bluesky/export_data.sh
@@ -3,14 +3,14 @@
 # Export all data collected during Bluesky Case Studies course
 
 CATALOG=class_2021_03
-OUTPUT_DIR=./data_export
+OUTPUT_DIR=./${CATALOG}
 OPTIONS=
 OPTIONS+=" --all"
 OPTIONS+=" --copy-external"
 
-LOG_FILE=./export_data.log
+LOG_FILE=./${CATALOG}.log
 
 databroker-pack ${OPTIONS} ${CATALOG} ${OUTPUT_DIR} 2>&1 | tee ${LOG_FILE}
 
-tar czf ./data_export.tar.gz ./data_export
-/bin/rm -rf ./data_export
+tar czf ./${CATALOG}.tar.gz ./${CATALOG}
+/bin/rm -rf ./${CATALOG}
