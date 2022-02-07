33f308692d44f40073e518acc643c836b898b430
Pete R Jemian <prjemian@users.noreply.github.com>
2022-02-07 13:36:33 -0600

    MNT enable userCalcoutN

--- bluesky/instrument/devices/calculation_records.py
+++ bluesky/instrument/devices/calculation_records.py
@@ -18,7 +18,13 @@ GP_IOC_PREFIX = os.environ.get("GP_IOC_PREFIX", "gp:")
 calcs = apstools.synApps.UserCalcsDevice(GP_IOC_PREFIX, name="calcs")
 calcouts = apstools.synApps.UserCalcoutDevice(GP_IOC_PREFIX, name="calcouts")
 
+# Normally, do not do _any_ actions (like these) in the instrument
+# package since that might affect other simultaneous use.  In this
+# case, the actions are probably OK.  Most users forget they even exist.
+# These steps enable all the userCalcN and userCalcoutN records to process.
 calcs.enable.put(1)
+calcouts.enable.put(1)
 
+# record all calculation signals before & after each run
 sd.baseline.append(calcs)
 sd.baseline.append(calcouts)
