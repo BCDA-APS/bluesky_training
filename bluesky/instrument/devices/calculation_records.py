b11af7687df72fb5b33ca69fa3a2f329f84ab62c
Pete R Jemian <prjemian@gmail.com>
2022-03-29 12:49:54 -0500

    MNT #56

--- bluesky/instrument/devices/calculation_records.py
+++ bluesky/instrument/devices/calculation_records.py
@@ -18,12 +18,13 @@ IOC = iconfig.get("GP_IOC_PREFIX", "gp:")
 calcs = apstools.synApps.UserCalcsDevice(IOC, name="calcs")
 calcouts = apstools.synApps.UserCalcoutDevice(IOC, name="calcouts")
 
-# Normally, do not do _any_ actions (like these) in the instrument
-# package since that might affect other simultaneous use.  In this
-# case, the actions are probably OK.  Most users forget they even exist.
-# These steps enable all the userCalcN and userCalcoutN records to process.
-calcs.enable.put(1)
-calcouts.enable.put(1)
+if iconfig.get("ENABLE_CALCS", False):
+    # Normally, do not do _any_ actions (like these) in the instrument
+    # package since that might affect other simultaneous use.  In this
+    # case, the actions are probably OK.  Most users forget they even exist.
+    # These steps enable all the userCalcN and userCalcoutN records to process.
+    calcs.enable.put(1)
+    calcouts.enable.put(1)
 
 # record all calculation signals before & after each run
 sd.baseline.append(calcs)
