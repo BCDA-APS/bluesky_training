50a2daa5b2bbe9302721580007317e49536c01b6
Pete R Jemian <prjemian@gmail.com>
2022-05-02 16:59:03 -0500

    MNT #62

--- bluesky/instrument/queueserver.py
+++ bluesky/instrument/queueserver.py
@@ -25,6 +25,9 @@ import pyRestTable
 
 
 def print_devices_and_signals():
+    """
+    Print the Devices and Signals in the current global namespace.
+    """
     glo = globals().copy()
 
     table = pyRestTable.Table()
@@ -43,6 +46,9 @@ def print_devices_and_signals():
 
 
 def print_plans():
+    """
+    Print the plans in the current global namespace.
+    """
     glo = globals().copy()
     plans = [
         k
@@ -55,52 +61,6 @@ def print_plans():
             print(f"* {k}{inspect.signature(glo[k])}")
         print("")
 
-
-def startup_report():
-    """Show instrument data on startup"""
-    from . import iconfig
-    from ophyd import Device, Signal
-    import inspect
-    import pyRestTable
-
-    table = pyRestTable.Table()
-    table.labels = "key value".split()
-    for k, v in sorted(iconfig.items()):
-        table.addRow((k, v))
-    if len(table.rows) > 0:
-        print("")
-        print("Instrument configuration (iconfig):")
-        print(table)
-
-    glo = globals().copy()
-
-    table = pyRestTable.Table()
-    table.labels = "device/object pvprefix/pvname connected?".split()
-    for k, v in sorted(glo.items()):
-        if isinstance(v, (Device, Signal)) and not k.startswith("_"):
-            v.wait_for_connection()
-            p = ""
-            for aname in "pvname prefix".split():
-                if hasattr(v, aname):
-                    p = getattr(v, aname)
-            table.addRow((v.name, p, v.connected))
-    if len(table.rows) > 0:
-        print("Table of Devices and signals:")
-        print(table)
-
-    plans = [
-        k
-        for k, v in sorted(glo.items()) 
-        if inspect.isgeneratorfunction(v)
-    ]
-    if len(plans) > 0:
-        print("List of Plans:")
-        for k in plans:
-            print(f"* {k}{inspect.signature(glo[k])}")
-        print("")
-
-
-# startup_report()
 
 if iconfig.get("APS_IN_BASELINE", False):
     sd.baseline.append(aps)
