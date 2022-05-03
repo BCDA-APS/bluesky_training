6670320057242a8a67adbcfa011c18128b6e5431
Pete R Jemian <prjemian@gmail.com>
2022-05-02 21:02:46 -0500

    TST #43 first unit tests

---
+++ bluesky/tests/test_instrument_configuration.py
@@ -0,0 +1,32 @@
+import pathlib
+import yaml
+
+
+ROOT_PATH = (pathlib.Path(__file__).parent.parent).absolute()
+ICONFIG_YAML_FILE = ROOT_PATH / "instrument" / "iconfig.yml"
+
+
+def test_existential():
+    assert ROOT_PATH.exists()
+    assert ICONFIG_YAML_FILE.exists()
+
+
+def test_contents():
+    iconfig = yaml.load(open(ICONFIG_YAML_FILE, "r").read(), yaml.Loader)
+
+    assert iconfig is not None
+    assert isinstance(iconfig, dict)
+    assert len(iconfig) > 5
+
+    key_list = """
+    DATABROKER_CATALOG
+    MINIMUM_BLUESKY_VERSION
+    MINIMUM_DATABROKER_VERSION
+    MINIMUM_OPHYD_VERSION
+    MINIMUM_PYTHON_VERSION
+    PV_CONNECTION_TIMEOUT
+    PV_TIMEOUT
+    PV_WRITE_TIMEOUT
+    """.strip().split()
+    for key in key_list:
+        assert key in iconfig
