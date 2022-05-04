db74c196469bf0c78320ce2714e044f55de58dd6
Pete R Jemian <prjemian@gmail.com>
2022-05-03 19:48:59 -0500

    TST #43

---
+++ bluesky/tests/test_bluesky_plans.py
@@ -0,0 +1,45 @@
+from bluesky_queueserver_api import BPlan
+from bluesky_queueserver_api.zmq import REManagerAPI
+from bluesky_queueserver_api.comm_base import RequestFailedError
+import pytest
+import time
+
+
+RM = REManagerAPI()
+
+
+def test_env_open():
+    try:
+        RM.environment_close()
+        RM.wait_for_idle()
+    except RequestFailedError:
+        pass
+    RM.environment_open()
+    RM.wait_for_idle()
+
+    status = RM.status()
+    for key in "re_state manager_state worker_environment_state".split():
+        assert key in status
+        assert status[key] == "idle"
+
+
+@pytest.mark.parametrize(
+    "plan, items",
+    [
+        ["findpeak_multipass", 1],
+        ["repeat_findpeak", 1],
+        ["two_pass_scan", 1],
+    ]
+)
+def test_findpeak_multipass(plan, items):
+    RM.queue_clear()
+    RM.history_clear()
+    RM.item_add(BPlan(plan))
+    assert RM.status()["items_in_queue"] == 1
+
+    RM.queue_start()
+    time.sleep(2)
+    assert RM.status()["re_state"] != "idle"
+
+    RM.wait_for_idle()
+    assert RM.status()["items_in_history"] == items
--- bluesky/tests/test_queueserver_startup.py
+++ bluesky/tests/test_queueserver_startup.py
@@ -6,7 +6,7 @@ sys.path.append(str(ROOT_PATH))
 
 
 def test_startup(capsys):
-    from starter import iconfig
+    from qstarter import iconfig
 
     out, err = capsys.readouterr()
 
