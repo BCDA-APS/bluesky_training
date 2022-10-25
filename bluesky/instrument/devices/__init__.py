c86ff4e20dcb78e0d56ea469950f6a0b3dda02d7
Pete R Jemian <prjemian@gmail.com>
2022-10-25 14:05:18 -0500

    DOC note about ophyd logging

--- bluesky/instrument/devices/__init__.py
+++ bluesky/instrument/devices/__init__.py
@@ -2,6 +2,13 @@
 local, custom Device definitions
 """
 
+# from ophyd.log import config_ophyd_logging
+# config_ophyd_logging(level="DEBUG")
+    # 'ophyd' — the logger to which all ophyd log records propagate
+    # 'ophyd.objects' — logs records from all devices and signals (that is, OphydObject subclasses)
+    # 'ophyd.control_layer' — logs requests issued to the underlying control layer (e.g. pyepics, caproto)
+    # 'ophyd.event_dispatcher' — issues regular summaries of the backlog of updates from the control layer that are being processed on background threads
+
 # from .aps_source import *
 # from .aps_undulator import *
 
