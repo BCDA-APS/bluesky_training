133b35e1804163fc5dba198155d2467385f28de6
Pete R Jemian <prjemian@gmail.com>
2023-01-18 15:48:40 -0600

    CI resolve one failure mode in testing

--- bluesky/instrument/collection.py
+++ bluesky/instrument/collection.py
@@ -17,9 +17,12 @@ from . import iconfig
 from IPython import get_ipython
 
 # terse error dumps (Exception tracebacks)
-_xmode_level = iconfig.get("XMODE_DEBUG_LEVEL", "Minimal")
-get_ipython().run_line_magic('xmode', _xmode_level)
-logger.info("xmode exception level: '%s'", _xmode_level)
+_ip = get_ipython()
+if _ip is not None:
+    _xmode_level = iconfig.get("XMODE_DEBUG_LEVEL", "Minimal")
+    _ip.run_line_magic('xmode', _xmode_level)
+    logger.info("xmode exception level: '%s'", _xmode_level)
+    del _ip
 
 from . import mpl
 
