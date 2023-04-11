d41d0a0fd0cddde3860096653e67577c95f9c7b5
Pete R Jemian <prjemian@gmail.com>
2023-04-11 17:44:06 -0500

    DOC #76 sphinx setup

---
+++ docs/source/conf.py
@@ -0,0 +1,61 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'bluesky_training'
+copyright = '2023, APS BCDA'
+author = 'APS BCDA'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = """
+    IPython.sphinxext.ipython_console_highlighting
+    IPython.sphinxext.ipython_directive
+    sphinx.ext.autodoc
+    sphinx.ext.autosummary
+    sphinx.ext.coverage
+    sphinx.ext.githubpages
+    sphinx.ext.inheritance_diagram
+    sphinx.ext.mathjax
+    sphinx.ext.todo
+    sphinx.ext.viewcode
+    nbsphinx
+    myst_parser
+""".split()
+
+templates_path = ["_templates"]
+source_suffix = [".rst", ".md"]
+exclude_patterns = ["**.ipynb_checkpoints"]
+
+today_fmt = "%Y-%m-%d %H:%M"
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_static_path = ['_static']
+html_theme = "pydata_sphinx_theme"
+
+autodoc_mock_imports = """
+    bluesky
+    dask
+    databroker
+    databroker_pack
+    epics
+    h5py
+    intake
+    numpy
+    openpyxl
+    ophyd
+    pandas
+    pint
+    psutil
+    pyRestTable
+    pysumreg
+    spec2nexus
+    xarray
+""".split()
---
+++ docs/source/index.rst
@@ -0,0 +1,21 @@
+.. bluesky_training documentation master file, created by
+   sphinx-quickstart on Tue Apr 11 14:58:56 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to bluesky_training's documentation!
+============================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   howto/index
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
