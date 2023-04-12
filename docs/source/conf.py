b15a03dc35551ce3b1836aa8a60ba3bcb72e464e
Pete R Jemian <prjemian@gmail.com>
2023-04-12 17:58:12 -0500

    DOC #95 copy conf from #76

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,13 +13,52 @@ author = "APS BCDA"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = """
+    IPython.sphinxext.ipython_console_highlighting
+    IPython.sphinxext.ipython_directive
+    sphinx.ext.autodoc
+    sphinx.ext.autosummary
+    sphinx.ext.coverage
+    sphinx.ext.githubpages
+    sphinx.ext.inheritance_diagram
+    sphinx.ext.mathjax
+    sphinx.ext.todo
+    sphinx.ext.viewcode
+    nbsphinx
+    myst_parser
+""".split()
 
 templates_path = ["_templates"]
-exclude_patterns = []
+source_suffix = ".rst .md".split()
+exclude_patterns = ["**.ipynb_checkpoints"]
+
+# today_fmt = "%Y-%m-%d %H:%M"
+
+# Ignore errors in notebooks while documenting them
+nbsphinx_allow_errors = True
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_static_path = ["_static"]
 html_theme = "pydata_sphinx_theme"
+
+autodoc_mock_imports = """
+    bluesky
+    dask
+    databroker
+    databroker_pack
+    epics
+    h5py
+    intake
+    numpy
+    openpyxl
+    ophyd
+    pandas
+    pint
+    psutil
+    pyRestTable
+    pysumreg
+    spec2nexus
+    xarray
+""".split()
