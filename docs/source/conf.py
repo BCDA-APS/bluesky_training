e8f5059a2e448c1f3621e78e8b61ad863ca06f96
Pete R Jemian <prjemian@gmail.com>
2023-04-12 17:52:01 -0500

    CI #95

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,5 +21,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
 html_static_path = ['_static']
+html_theme = "pydata_sphinx_theme"
