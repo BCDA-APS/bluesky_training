35e2c37d5585f7078ab10af9f45ed9864b1857c6
Pete R Jemian <prjemian@gmail.com>
2023-04-12 17:52:47 -0500

    STY #95 apply flake8 & black

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,20 +6,20 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'APS Bluesky Training'
-copyright = '2023, APS BCDA'
-author = 'APS BCDA'
+project = "APS Bluesky Training"
+copyright = "2023, APS BCDA"
+author = "APS BCDA"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_static_path = ['_static']
+html_static_path = ["_static"]
 html_theme = "pydata_sphinx_theme"
