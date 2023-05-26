acba7298c23a561e16764c3358c50b3ed6260971
Pete R Jemian <prjemian@gmail.com>
2023-05-26 14:37:28 -0500

    MNT #130

--- bluesky/_run_qs.sh
+++ bluesky/_run_qs.sh
@@ -2,12 +2,31 @@
 
 # Start the bluesky queueserver.
 
+SHELL_SCRIPT_NAME=${BASH_SOURCE:-${0}}
+if [ -z "$STARTUP_DIR" ] ; then
+    # If no startup dir is specified, use the directory with this script
+    export STARTUP_DIR=$(dirname "${SHELL_SCRIPT_NAME}")
+fi
+
 #--------------------
 # change the program defaults here
-# TODO: CONDA_ENVIRONMENT="${BLUESKY_CONDA_ENV:-training_2022}"
-CONDA_ENVIRONMENT=training_2022
-DATABROKER_CATALOG=training
+export CONDA_ENVIRONMENT="${BLUESKY_CONDA_ENV:-training_2022}"
+export DATABROKER_CATALOG=${DATABROKER_CATALOG:-training}
+export QS_SERVER_HOST=$(hostname)
+export QS_UPDATE_PLANS_DEVICES=ENVIRONMENT_OPEN
+export QS_USER_GROUP_PERMISSIONS_FILE="${STARTUP_DIR}/user_group_permissions.yaml"
+export QS_USER_GROUP_PERMISSIONS_RELOAD=ON_STARTUP
 #--------------------
+
+# REDIS_ADDR is __always__ localhost.
+# Override if it is not, but you may encounter access issues.  YOYO.
+export REDIS_ADDR=localhost
+
+# QS and redis must be on the same workstation
+if [ "$(hostname)" != "${QS_SERVER_HOST}" ]; then
+    echo "Must run queueserver on ${QS_SERVER_HOST}.  This is $(hostname)"
+    exit 1
+fi
 
 # activate conda environment
 
@@ -26,7 +45,7 @@ if [ "${CONDA}" == "" ] ; then
         fi
     fi
 fi
-CONDA_BASE_DIR="${CONDA}/bin"
+CONDA_BASE_BIN="${CONDA}/bin"
 
 # In GitHub Actions workflow,
 # $ENV_NAME is an environment variable naming the conda environment to be used
@@ -36,17 +55,13 @@ fi
 
 # echo "Environment: $(env | sort)"
 
-source "${CONDA_BASE_DIR}/activate" "${ENV_NAME}"
-
-SHELL_SCRIPT_NAME=${BASH_SOURCE:-${0}}
-if [ -z "$STARTUP_DIR" ] ; then
-    # If no startup dir is specified, use the directory with this script
-    STARTUP_DIR=$(dirname "${SHELL_SCRIPT_NAME}")
-fi
+source "${CONDA_BASE_BIN}/activate" "${ENV_NAME}"
 
 start-re-manager \
+    --redis-addr "${REDIS_ADDR}" \
     --startup-dir "${STARTUP_DIR}" \
-    --update-existing-plans-devices ENVIRONMENT_OPEN \
+    --update-existing-plans-devices "${QS_UPDATE_PLANS_DEVICES}" \
+    --user-group-permissions "${QS_USER_GROUP_PERMISSIONS_FILE}" \
+    --user-group-permissions-reload "${QS_USER_GROUP_PERMISSIONS_RELOAD}" \
     --zmq-publish-console ON \
     --keep-re
-    # --databroker-config "${DATABROKER_CATALOG}"
