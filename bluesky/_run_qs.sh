67e89dd93ae35e4267474d32ad42dcf3791f4173
Pete R Jemian <prjemian@gmail.com>
2023-05-26 16:20:10 -0500

    DOC #130

--- bluesky/_run_qs.sh
+++ bluesky/_run_qs.sh
@@ -8,7 +8,7 @@
 export CONDA=${CONDA:-/APSshare/miniconda/x86_64}
 export CONDA_ENVIRONMENT="${BLUESKY_CONDA_ENV:-training_2022}"
 export DATABROKER_CATALOG=${DATABROKER_CATALOG:-training}
-export QS_SERVER_HOST=$(hostname)
+export QS_SERVER_HOST=$(hostname)  # or host (that passes $(hostname) test below)
 export QS_UPDATE_PLANS_DEVICES=ENVIRONMENT_OPEN
 export QS_USER_GROUP_PERMISSIONS_FILE="./user_group_permissions.yaml"
 export QS_USER_GROUP_PERMISSIONS_RELOAD=ON_STARTUP
